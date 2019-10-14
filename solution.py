30cddd21381bd9f37dc996b4ec56c7f9b72c88c1
Andrey <andreylevchuk8900@gmail.com>
2019-10-14 12:11:46 +0300

    Refractor DataTransfer class.

--- solution.py
+++ solution.py
@@ -5,29 +5,14 @@ from XMLHandler import XMLHandler
 
 class DataTransfer():
 
-    def __init__(self):
-        self._in_data_handler = None
-        self._out_data_handler = None
-    
-    in_data_handler = property()
-    out_data_handler = property()
-    
-    @in_data_handler.setter
-    def in_data_handler(self, value):
-        self._in_data_handler = value
+    def read_rooms(self, handler, filename):
+        self.rooms = handler.load(filename)
 
-    @out_data_handler.setter
-    def out_data_handler(self, value):
-        self._out_data_handler = value
+    def read_students(self, handler, filename):
+        self.students = handler.load(filename)
 
-    def read_rooms(self, filename):
-        self.rooms = self._in_data_handler.load(filename)
-
-    def read_students(self, filename):
-        self.students = self._in_data_handler.load(filename)
-
-    def write_to_file(self, filename):
-        self._out_data_handler.dump(self.data, filename)
+    def write_to_file(self, handler, filename):
+        handler.dump(self.data, filename)
 
     def handle_data(self):
         rooms_dict = {i['id']: {'name': i['name'], 'students':[]} for i in self.rooms}
@@ -39,26 +24,24 @@ class DataTransfer():
 
 
 def main(students_filename, rooms_filename, out_format):
+    
     handler = DataTransfer()
-    handler.in_data_handler = JSONHandler()  
+     
+    try:
+        handler.read_rooms(JSONHandler, rooms_filename)
+        handler.read_students(JSONHandler, students_filename)    
+    except FileNotFoundError as e:
+        return
+
+    handler.handle_data()
 
     if out_format == 'json':
-        handler.out_data_handler = JSONHandler()  
+        handler.write_to_file(JSONHandler, 'out.'+out_format)
     elif out_format == 'xml':
-        handler.out_data_handler = XMLHandler()
+         handler.write_to_file(XMLHandler, 'out.'+out_format)
     else: 
-        return
-         
-    try:
-        handler.read_rooms(rooms_filename)
-        handler.read_students(students_filename)    
-    except FileNotFoundError as e:
-        print(e)
-        return
-
-    handler.handle_data()  
-    handler.write_to_file('out.'+out_format)
-
+        return  
+    
 
 if __name__ == '__main__':
 
